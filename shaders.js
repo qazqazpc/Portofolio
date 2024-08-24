47b49d5f112da1432469d6a85bd5250713eee4a5
qazqazpc <167785256+qazqazpc@users.noreply.github.com>
2024-08-24 21:40:04 +0700

    Update shaders.js

--- shaders.js
+++ shaders.js
@@ -799,4 +799,4 @@ window.addEventListener('load', function(e) {
 //set window.requestAnimationFrame
 (function (w, r) {
     w['r'+r] = w['r'+r] || w['webkitR'+r] || w['mozR'+r] || w['msR'+r] || w['oR'+r] || function(c){ w.setTimeout(c, 1000 / 60); };
-})(window, 'equestAnimationFrame');
+})(window, 'requestAnimationFrame');
